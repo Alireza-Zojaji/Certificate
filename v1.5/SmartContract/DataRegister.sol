75642d552c7fa3f7135dae3087a6ed4de9e6259e
Alireza Zojaji <38731072+Alireza-Zojaji@users.noreply.github.com>
2018-12-09 13:19:07 +0330

    Create DataRegister.sol

---
+++ v1.5/SmartContract/DataRegister.sol
@@ -0,0 +1,167 @@
+pragma solidity ^0.5.1;
+import './Operations.sol';
+contract DataRegister is Operations {
+    bytes32 Institute; 
+    address owner;
+    mapping(bytes10 => bytes) Instructor;
+    mapping(uint => bytes10) InstructorUIds;
+    uint InstructorCount = 0;
+    struct course {
+//        bytes10 CourseNumber;
+        bytes CourseName;
+        bytes10 StartDate;
+        bytes10 EndDate;
+        uint Hours;
+        uint InstructorId;
+    }
+    mapping(bytes10 => course) Course;
+    mapping(uint => bytes10) CourseUIds;
+    uint CourseCount = 0;
+    struct student {
+        bytes Name;
+        bytes10 NationalId;
+    }
+    mapping(bytes10 => student) Student;
+    mapping(uint => bytes10) StudentUIds;
+    uint StudentCount = 0;
+    struct certificate {
+        uint CourseId;
+        uint StudentId;
+        uint CertificateType;
+        bytes10 Result;
+        bool Enabled;
+    }
+    mapping(bytes10 => certificate) Certificate;
+    uint CertificateCount = 0;
+    mapping(uint => bytes10) CertificateUIds;
+    modifier onlyOwner() {
+        require(msg.sender==owner);
+        _;
+    }
+    modifier notEmpty(string memory str) {
+        bytes memory bStr = bytes(str);
+        require(bStr.length > 0);
+        _;
+    }
+    modifier isPositive(uint number) {
+        require(number > 0);
+        _;
+    }
+    modifier haveInstructor(uint InstructorId) {
+        require(Instructor[InstructorUIds[InstructorId]].length > 0);
+        _;
+    }
+    modifier haveCourse(uint CourseId) {
+        require(CourseUIds[CourseId].length > 0);
+        _;
+    }
+    modifier haveStudent(uint StudentId) {
+        require(Student[StudentUIds[StudentId]].Name.length > 0);
+        _;
+    }
+    modifier uniqueCertificateUId(string memory certificateUId) {
+        require(Certificate[bytes10(stringToBytes32(certificateUId))].CourseId == 0);
+        _;
+    }
+    modifier uniqueInstructorUId(string memory _instructorUId) {
+        require(Instructor[bytes10(stringToBytes32(_instructorUId))].length == 0);
+        _;
+    }
+    modifier uniqueCourseUId(string memory _courseUId) {
+        require(Course[bytes10(stringToBytes32(_courseUId))].CourseName.length == 0);
+        _;
+    }
+    modifier uniqueStudentUId(string memory _studentUId) {
+        require(Student[bytes10(stringToBytes32(_studentUId))].Name.length == 0);
+        _;
+    }
+    function RegisterInstructor(
+        string memory instructorUId, 
+        string memory instructor
+        ) public onlyOwner notEmpty(instructorUId) notEmpty(instructor) uniqueInstructorUId(instructorUId) returns(bool) {
+            bytes10 _instructorUId = bytes10(stringToBytes32(instructorUId));
+            InstructorCount++;
+            Instructor[_instructorUId] = bytes(instructor);
+            InstructorUIds[InstructorCount]=_instructorUId;
+            return(true);
+    }
+    function RegisterCourse(
+        string memory CourseUId,
+        string memory CourseName,
+        string memory StartDate,
+        string memory EndDate,
+        uint Hours,
+        uint InstructorId
+        ) public onlyOwner notEmpty(CourseUId) notEmpty(CourseName) 
+            isPositive(Hours) haveInstructor(InstructorId) uniqueCourseUId(CourseUId) {
+            course memory _course = setCourseElements(CourseName, StartDate, EndDate, Hours, InstructorId);
+            CourseCount++;
+            bytes10 _courseUId = bytes10(stringToBytes32(CourseUId));
+            CourseUIds[CourseCount] = _courseUId;
+            Course[_courseUId] = _course;
+    }
+    function setCourseElements(
+        string memory CourseName, 
+        string memory StartDate, 
+        string memory EndDate,
+        uint Hours,
+        uint InstructorId
+        ) internal pure returns(course memory) {
+        course memory _course;
+        _course.CourseName = bytes(CourseName);
+        _course.StartDate = bytes10(stringToBytes32(StartDate));
+        _course.EndDate = bytes10(stringToBytes32(EndDate));
+        _course.Hours = Hours;
+        _course.InstructorId = InstructorId;
+        return(_course);
+    }
+    function RegisterStudent(
+        string memory StudentUId,
+        string memory Name,
+        string memory NationalId
+        ) public onlyOwner notEmpty(Name) notEmpty(NationalId) notEmpty(StudentUId) uniqueStudentUId(StudentUId) returns(bool) {
+            StudentCount++;
+            StudentUIds[StudentCount] = bytes10(stringToBytes32(StudentUId));
+            student memory _student;
+            _student.Name = bytes(Name);
+            _student.NationalId = bytes10(stringToBytes32(NationalId));
+            Student[StudentUIds[StudentCount]]=_student;
+        return(true);
+    }
+    function RegisterCertificate(
+        string memory CertificateUId,
+        uint CourseId,
+        uint StudentId,
+        uint CertificateType,
+        string memory Result
+        ) public onlyOwner haveStudent(StudentId) haveCourse(CourseId) 
+        uniqueCertificateUId(CertificateUId) isPositive(CertificateType) returns(bool) {
+            certificate memory _certificate;
+            _certificate.CourseId = CourseId;
+            _certificate.StudentId = StudentId;
+            _certificate.CertificateType = CertificateType;
+            _certificate.Result = bytes10(stringToBytes32(Result));
+            _certificate.Enabled = true;
+            bytes10 cert_uid = bytes10(stringToBytes32(CertificateUId));
+            CertificateCount++;
+            Certificate[cert_uid] = _certificate;
+            CertificateUIds[CertificateCount] = cert_uid;
+            return(true);
+    }
+    function EnableCertificate(string memory CertificateId) public onlyOwner notEmpty(CertificateId) returns(bool) {
+        bytes10 _certificateId = bytes10(stringToBytes32(CertificateId));
+        certificate memory _certificate = Certificate[_certificateId];
+        require(_certificate.Result != '');
+        require(! _certificate.Enabled);
+        Certificate[_certificateId].Enabled = true;
+        return(true);
+    }
+    function DisableCertificate(string memory CertificateId) public onlyOwner notEmpty(CertificateId) returns(bool) {
+        bytes10 _certificateId = bytes10(stringToBytes32(CertificateId));
+        certificate memory _certificate = Certificate[_certificateId];
+        require(_certificate.Result != '');
+        require(_certificate.Enabled);
+        Certificate[_certificateId].Enabled = false;
+        return(true);
+    }
+}
